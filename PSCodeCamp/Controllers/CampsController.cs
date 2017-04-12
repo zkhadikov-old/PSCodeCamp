7bb57722d7207aefc30b707c3b2cf11f105b62b8
zkhadikov <kvzura@gmail.com>
2017-04-12 22:19:50 +0200

    finish update and delete operation for api

--- PSCodeCamp/Controllers/CampsController.cs
+++ PSCodeCamp/Controllers/CampsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MyCodeCamp.Data;
 using MyCodeCamp.Data.Entities;
 using System;
@@ -12,10 +13,12 @@ namespace PSCodeCamp.Controllers
     public class CampsController : Controller
     {
         private ICampRepository _reposotory;
+        private ILogger<CampsController> _logger;
 
-        public CampsController(ICampRepository repository)
+        public CampsController(ICampRepository repository, ILogger<CampsController> logger)
         {
             _reposotory = repository;
+            _logger = logger;
         }
 
         [HttpGet("")]
@@ -63,18 +66,88 @@ namespace PSCodeCamp.Controllers
         {
             try
             {
+                _logger.LogInformation("Create a new Code Camp");
+
                 _reposotory.Add(model);
                 if (await _reposotory.SaveAllAsync())
                 {
                     var newUri = Url.Link("CampGet", new { id = model.Id });
                     return Created(newUri, model);
                 }
+                else
+                {
+                    _logger.LogWarning("Could not save Camp to the database");
+                }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                _logger.LogError($"Threw exception thile saving Camp: {ex}");
             }
 
             return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Camp model)
+        {
+            try
+            {
+                var oldCamp = _reposotory.GetCamp(id);
+                if (oldCamp == null)
+                {
+                    return NotFound($"Could not found a camp with an ID of {id}");
+                }
+
+                // map model to the oldcamp
+                oldCamp.Name = model.Name ?? oldCamp.Name;
+                oldCamp.Description = model.Description ?? oldCamp.Description;
+                oldCamp.Location = model.Location ?? oldCamp.Location;
+                oldCamp.Length = model.Length > 0 ? model.Length : oldCamp.Length;
+
+                if (await _reposotory.SaveAllAsync())
+                {
+                    return Ok(oldCamp);
+                }
+                else
+                {
+                    _logger.LogWarning("Could not save Camp to the database");
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return BadRequest("Couldn't update Camp");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var oldCamp = _reposotory.GetCamp(id);
+                if (oldCamp == null)
+                {
+                    return NotFound($"Could not found a camp with an ID of {id}");
+                }
+
+                _reposotory.Delete(oldCamp);
+
+                if (await _reposotory.SaveAllAsync())
+                {
+                    return Ok();
+                }
+            }
+            catch (System.Exception)
+            {
+                
+                throw;
+            }
+
+            return BadRequest("Couldn't delete Camp");
         }
     }
 }
