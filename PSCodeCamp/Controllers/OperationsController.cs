5862d3e487c7282818b8ee5b5665019364c63d9f
zkhadikov <kvzura@gmail.com>
2017-04-26 23:03:47 +0200

    finish functional api example

---
+++ PSCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class OperationsController : Controller 
+    {
+        private ILogger<OperationsController> _logger;
+        private IConfigurationRoot _config;
+
+        public OperationsController(ILogger<OperationsController> logger, IConfigurationRoot config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpOptions("reloadConfig")]
+        public IActionResult ReloadConfiguration()
+        {
+            try
+            {
+                _config.Reload();
+
+                return Ok("Configuration Reloaded");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception thrown while reloading configuration: {ex}");
+            }
+
+            return BadRequest("Could not reload configuration");
+        }
+    }
+}
