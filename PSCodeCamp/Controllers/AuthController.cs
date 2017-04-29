f2c5ab9fe56cc1379cb92df144ffaee9fc4f9717
zkhadikov <kvzura@gmail.com>
2017-04-29 23:46:57 +0200

    add JWT Token creation

--- PSCodeCamp/Controllers/AuthController.cs
+++ PSCodeCamp/Controllers/AuthController.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using MyCodeCamp.Data;
 using MyCodeCamp.Data.Entities;
 using PSCodeCamp.Filters;
 using PSCodeCamp.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PSCodeCamp.Controllers
@@ -16,17 +18,25 @@ namespace PSCodeCamp.Controllers
     {
         private CampContext _context;
         private SignInManager<CampUser> _signInManager;
+        private UserManager<CampUser> _userManager;
+        private IPasswordHasher<CampUser> _hasher;
         private ILogger<AuthController> _logger;
 
-        public AuthController(CampContext context, SignInManager<CampUser> signInManager, ILogger<AuthController> logger)
+        public AuthController(CampContext context,
+            SignInManager<CampUser> signInManager,
+            UserManager<CampUser> userManager,
+            IPasswordHasher<CampUser> hasher,
+            ILogger<AuthController> logger)
         {
             _context = context;
             _signInManager = signInManager;
+            _userManager = userManager;
+            _hasher = hasher;
             _logger = logger;
         }
 
-        [HttpPost("api/auth/login")]
         [ValidateModel]
+        [HttpPost("api/auth/login")]
         public async Task<IActionResult> Login([FromBody]CredentialModel model)
         {
             try
@@ -44,6 +54,51 @@ namespace PSCodeCamp.Controllers
             }
 
             return BadRequest("Failed to login");
+        }
+
+        [ValidateModel]
+        [HttpPost("api/auth/token")]
+        public async Task<IActionResult> CreateToken([FromBody]CredentialModel model)
+        {
+            try
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+
+                if (user != null)
+                {
+                    if (_hasher.VerifyHashedPassword(user, user.PasswordHash, model.Password) == PasswordVerificationResult.Success)
+                    {
+                        var claims = new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                        };
+
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MYVERYLONGSECURITYKEY"));
+                        var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                        var token = new JwtSecurityToken(
+                            issuer: "http://www.jedox.com",
+                            audience: "http://www.jedox.com",
+                            claims: claims,
+                            expires: DateTime.UtcNow.AddMinutes(15),
+                            signingCredentials: cred);
+
+                        return Ok(new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            expiration = token.ValidTo
+
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception thrown while creating JWT: {ex}");
+            }
+
+            return BadRequest("Failed to generate token");
         }
     }
 }
